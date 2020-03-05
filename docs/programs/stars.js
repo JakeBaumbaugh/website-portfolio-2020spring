3e7528d5e4e9e73242ace56b3abcf202951f4a99
Jake Jake <jake.baumbaugh@gmail.com>
2020-03-05 15:58:03 -0500

    Added stars project, not polished

---
+++ docs/programs/stars.js
@@ -0,0 +1,45 @@
+var s = [];
+
+function setup() {
+	var canvas = createCanvas(720, 480);
+	canvas.parent("program");
+	for(var i = 0; i < 150; i++) {
+		s[i] = new Star();
+	}
+	strokeWeight(3);
+	frameRate(30);
+}
+
+function draw() {
+	background(16);
+	
+	translate(width/2, height/2);
+	var currTime = millis();
+	for(var i = 0; i < s.length; i++) {
+		var needNewStar = s[i].show(currTime);
+		if(needNewStar) {
+			s[i] = new Star();
+		}
+	}
+}
+
+class Star {
+	
+	constructor() {
+		this.theta = random(0, TWO_PI);
+		this.dist = random(1, 10);
+		this.startTime = millis();
+	}
+	
+	show(currTime) {
+		var refPoint = createVector(cos(this.theta), sin(this.theta));
+		var time = currTime - this.startTime;
+		var startPoint = refPoint.mult(time*0.3);
+		var endPoint = p5.Vector.mult(startPoint, time/1000*sqrt(this.dist));
+		stroke(map(this.dist, 1, 10, 64, 255));
+		strokeWeight(ceil(this.dist/3));
+		line(startPoint.x, startPoint.y, endPoint.x, endPoint.y);
+		
+		return abs(startPoint.x)>width/2 || abs(startPoint.y)>height/2;
+	}
+}
