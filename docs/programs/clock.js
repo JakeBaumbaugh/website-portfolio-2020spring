d070c53db45c6b47f18c85f79bd66c143ac6b0e0
Jake Jake <jake.baumbaugh@gmail.com>
2020-03-24 15:27:56 -0400

    updated clock.js

--- docs/programs/clock.js
+++ docs/programs/clock.js
@@ -1,6 +1,8 @@
 function setup() {
   var canvas = createCanvas(720, 480);
   canvas.parent("program");
+	
+	noFill();
 }
 
 function draw() {
@@ -9,6 +11,22 @@ function draw() {
 	
 	background(32);
 	
-	stroke(255,255,192);
+	stroke(255,192,255);
+	strokeWeight(8);
 	ellipse(0,0,360,360);
+	
+	var h = -2*PI * ( (hour()%12) / 24)
+	stroke(255,192,192);
+	strokeWeight(4);
+	line(0,0,cos(h),sin(h));
+	
+	var m = -2*PI * ( minute() / 60)
+	stroke(192,255,192);
+	strokeWeight(2);
+	line(0,0,cos(m),sin(m));
+	
+	var s = -2*PI * ( second() / 60)
+	stroke(192,255,255);
+	strokeWeight(1);
+	line(0,0,cos(s),sin(s));
 }
