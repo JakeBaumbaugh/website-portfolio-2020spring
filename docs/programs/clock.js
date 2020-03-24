30b254ef3ba66bd6f09173d0de6f293eaf140283
Jake Jake <jake.baumbaugh@gmail.com>
2020-03-24 15:46:12 -0400

    changed clock colors in clock.js hopefully for the last time

--- docs/programs/clock.js
+++ docs/programs/clock.js
@@ -1,8 +1,10 @@
+var red = color(112,0,0);
+var yellow = color(192,116,32);
+var gray = color(32);
+
 function setup() {
   var canvas = createCanvas(720, 480);
   canvas.parent("program");
-	
-	noFill();
 }
 
 function draw() {
@@ -11,19 +13,21 @@ function draw() {
 	
 	background(32);
 	
-	stroke(192,116,32);
+	fill(255);
+	stroke(red);
 	strokeWeight(8);
 	ellipse(0,0,400,400);
 	
-	stroke(255,255,255);
+	stroke(gray);
 	var h = 2*PI * ( (hour()%12) / 12)
 	strokeWeight(4);
 	line(0,0,120*cos(h),120*sin(h));
 	
 	var m = 2*PI * ( minute() / 60)
-	strokeWeight(2);
+	strokeWeight(4);
 	line(0,0,180*cos(m),180*sin(m));
 	
+	stroke(red);
 	var s = 2*PI * ( second() / 60)
 	strokeWeight(2);
 	line(0,0,180*cos(s),180*sin(s));
