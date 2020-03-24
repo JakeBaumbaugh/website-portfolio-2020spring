305afb04c5234569eb59c9d590fee00fc7fb5637
Jake Jake <jake.baumbaugh@gmail.com>
2020-03-24 15:34:05 -0400

    updated clock.js to fix clock hands again

--- docs/programs/clock.js
+++ docs/programs/clock.js
@@ -15,18 +15,18 @@ function draw() {
 	strokeWeight(8);
 	ellipse(0,0,360,360);
 	
-	var h = -2*PI * ( (hour()%12) / 24)
+	var h = 2*PI * ( (hour()%12) / 24)
 	stroke(255,192,192);
 	strokeWeight(4);
-	line(0,0,180*cos(h),180*sin(h));
+	line(0,0,90*cos(h),90*sin(h));
 	
-	var m = -2*PI * ( minute() / 60)
+	var m = 2*PI * ( minute() / 60)
 	stroke(192,255,192);
 	strokeWeight(2);
-	line(0,0,320*cos(m),320*sin(m));
+	line(0,0,160*cos(m),160*sin(m));
 	
-	var s = -2*PI * ( second() / 60)
+	var s = 2*PI * ( second() / 60)
 	stroke(192,255,255);
 	strokeWeight(1);
-	line(0,0,320*cos(s),320*sin(s));
+	line(0,0,160*cos(s),160*sin(s));
 }
