68c1755d828c1ec3d1da754f64169ef9d5c2df3f
Jake Jake <jake.baumbaugh@gmail.com>
2020-03-24 20:57:05 -0400

    began circs.js

---
+++ docs/programs/circs.js
@@ -0,0 +1,17 @@
+function setup() {
+  var canvas = createCanvas(720, 480);
+  canvas.parent("program");
+}
+
+function draw() {
+	translate(width/2, height/2);
+	background(32);
+	
+	noFill();
+	stroke(255);
+	strokeWeight(1);
+	ellipse(0,0,400,400);
+	for(var i = 0; i < 2*PI; i+= PI/4) {
+		line(-200*cos(i), -200*sin(i), 200*cos(i), 200*sin(i));
+	}
+}
