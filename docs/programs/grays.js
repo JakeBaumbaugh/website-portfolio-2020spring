f32b8756311b03f42fbb635f8ac04c7c12231e45
Jake Jake <jake.baumbaugh@gmail.com>
2020-03-25 15:48:32 -0400

    add grays.html with grays.js

---
+++ docs/programs/grays.js
@@ -0,0 +1,18 @@
+function setup() {
+  var canvas = createCanvas(720, 480);
+  canvas.parent("program");
+	loadPixels();
+	
+	noStroke();
+	fill();
+}
+
+function draw() {
+	for(int i = 0; i < pixels.length; i++) {
+		pixels[i] = map(i%width, 0, width, 0, 255);
+	}
+	updatePixels();
+	
+	var x = map(sin(frameCount/100), -1, 1, 0, 640);
+	rect(x, 200, 80, 80);
+}
