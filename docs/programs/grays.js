c94a1ef0e53ebef7eacb0ec22dc7fc7eafab35a2
Jake Jake <jake.baumbaugh@gmail.com>
2020-03-25 15:58:52 -0400

    updated grays.js to move bkg into setup with PGraphics to hopefully optimize

--- docs/programs/grays.js
+++ docs/programs/grays.js
@@ -4,15 +4,17 @@ function setup() {
 	
 	noStroke();
 	fill(128);
+	
+	var pg = createGraphics(720,480);
+	pg.loadPixels();
+	for(var i = 0; i < width*height; i++) {
+		pg.pixels[i] = color(map(i%width, 0, width, 0, 255));
+	}
+	pg.updatePixels();
 }
 
 function draw() {
-	loadPixels();
-	for(var i = 0; i < pixels.length; i++) {
-		pixels[i] = color(map(i%width, 0, width, 0, 255));
-	}
-	updatePixels();
-	
+	image(pg,0,0);	
 	var x = map(sin(frameCount/100), -1, 1, 0, 640);
 	rect(x, 200, 80, 80);
 }
