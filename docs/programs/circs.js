f749fc0dacd9e4e81d61c9525c987cd483cfc0a5
Jake Jake <jake.baumbaugh@gmail.com>
2020-03-24 21:12:51 -0400

    first test of finished circs.js

--- docs/programs/circs.js
+++ docs/programs/circs.js
@@ -1,6 +1,8 @@
 function setup() {
   var canvas = createCanvas(720, 480);
   canvas.parent("program");
+	
+	stroke(255);
 }
 
 function draw() {
@@ -8,10 +10,28 @@ function draw() {
 	background(32);
 	
 	noFill();
-	stroke(255);
 	strokeWeight(1);
 	ellipse(0,0,400,400);
-	for(var i = 0; i < PI; i+= PI/8) {
-		line(-200*cos(i), -200*sin(i), 200*cos(i), 200*sin(i));
+	
+	for(var i = 0; i < 1; i+= 1/8) {
+		noFill();
+		strokeWeight(1);
+		var posCoord = createVector(200*cos(i*PI), 200*sin(i*PI));
+		var negCoord = createVector(-200*cos(i*PI), -200*sin(i*PI));
+		line(negCoord.x, negCoord.y, posCoord.x, posCoord.y);
+		
+		var t = PI*(i+frameCount/10);
+		var ler = sin(t);
+		var zero = createVector(0,0);
+		var dotCoord;
+		if(ler >= 0) {
+			dotCoord = zero.lerp(posCoord, ler)
+		} else {
+			dotCoord = zero.lerp(negCoord, -ler)
+		}
+		
+		fill(255);
+		noStroke();
+		ellipse(dotCoord.x, dotCoord.y, 10, 10);
 	}
 }
